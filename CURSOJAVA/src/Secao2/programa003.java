8183a5377de5331e9ad32ce6a9344bbcb98a30f9
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:25:39 -0300

    Update programa003.java

--- CURSOJAVA/src/Secao2/programa003.java
+++ CURSOJAVA/src/Secao2/programa003.java
@@ -1,5 +1,9 @@
 /*
- Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a raiz
+Wesley Elbert Assis
+*/
+
+/*
+Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a raiz
 quadrada do número. Se o número for negativo, mostre uma mensagem dizendo que o número
 é inválido
  */
