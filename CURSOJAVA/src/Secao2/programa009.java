7ad3f644299f649368585b7bcea06e4a95d2f41b
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:26:27 -0300

    Update programa009.java

--- CURSOJAVA/src/Secao2/programa009.java
+++ CURSOJAVA/src/Secao2/programa009.java
@@ -1,4 +1,8 @@
 /*
+Wesley Elbert Assis
+*/
+
+/*
 Faça um programa que leia 2 notas de um aluno, verifique se as notas
 são válidas e exiba na tela a média destas notas. Uma nota válida deve ser,
 obrigatoriamente,um  valor entre 0.0 e 10.0, onde caso a nota não possua 
