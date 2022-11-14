a305f862955a26b3eb2023b1ac831565f9e2c910
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:23:54 -0300

    Update programa018.java

--- CURSOJAVA/src/Secao3/programa018.java
+++ CURSOJAVA/src/Secao3/programa018.java
@@ -1,4 +1,8 @@
 /*
+Wesley Elbert Assis
+*/
+
+/*
 Escreva um algoritmo que leia certa quantidade de números e imprima 
 o maior deles e quantas vezes o maior número foi lido. A quantidade
 de números a serem lidos deve ser fornecida pelo usuário.
