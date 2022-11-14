f7b9ceeef6ef805cf6184bca535bbf085f4df272
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:27:46 -0300

    Update programa016.java

--- CURSOJAVA/src/Secao2/programa016.java
+++ CURSOJAVA/src/Secao2/programa016.java
@@ -1,4 +1,8 @@
 /*
+Wesley Elbert Assis
+*/
+
+/*
 Usando Switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima
 o dia da semana correspondente a este número. Isto é, domingo se 1, segunda - feira se 2
 e assim por diante.
