2ad6f92348a7a6ea8e83b0611792238a9219fd5d
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:25:55 -0300

    Update programa005.java

--- CURSOJAVA/src/Secao2/programa005.java
+++ CURSOJAVA/src/Secao2/programa005.java
@@ -1,4 +1,8 @@
 /*
+Wesley Elbert Assis
+*/
+
+/*
 Faça um programa que leia um número e,caso ele seja positivo,calcule e mostre:
 > O número digitado ao quadrado
 > A raiz quadrada do número digitado
