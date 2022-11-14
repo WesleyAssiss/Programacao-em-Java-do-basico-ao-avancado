5cd54ee640342b5ac1eaa86fe2858884c65ac70b
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:11:56 -0300

    Update programa010.java

--- CURSOJAVA/src/Secao3/programa010.java
+++ CURSOJAVA/src/Secao3/programa010.java
@@ -1,14 +1,26 @@
 /*
- Faça um programa que calule e mostre a som a dos 50 primeiros números pares
+ Faça um programa que calule e mostre a soma dos 50 primeiros números pares
  */
 package Secao3;
 
-/**
- *
- * @author wesle
- */
+import java.util.Scanner;
+
+
 public class programa010 {
     
+   public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int numero = 0, i = 0, menor = 0, par = 2;
+
+        while (i < 50) {
+            System.out.println(par);
+            i = i + 1;
+            par = par + 2;
+
+        }
+    }
+
 }
 
     
