015deb19a53ed24c3ddc55b87f79f890870ecd2b
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:28:08 -0300

    Update programa019.java

--- CURSOJAVA/src/Secao2/programa019.java
+++ CURSOJAVA/src/Secao2/programa019.java
@@ -1,4 +1,8 @@
 /*
+Wesley Elbert Assis
+*/
+
+/*
 Faça um programa que mostre ao usuário um menu com 4 opções de operações matemáticas
 ( as Básicas, por exemplo). O usuário escolhe uma das opções e o seu programa então pede 
 dois valores numéricos e realiza a operação, mostrando resultado e saindo.
